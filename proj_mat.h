f4a7d106241d6cf44d6fde6aa27ccd7f60eccf69
michaellmonaghan <michaellmonaghan@gmail.com>
2016-04-07 23:34:59 -0400

    Hope this math works

--- proj_mat.h
+++ proj_mat.h
@@ -23,15 +23,14 @@
 void arb_proj_mat(const float *a, const float *b, const float *c, const float *e, float n, float f, float *mat);
 
 /*
-  Calculates the projection matrix assuming that the screen lays on the z = 0 plane.
+  Calculates the projection matrix assuming that the screen lays on the xy plane. everything behind it in -z will be in view.
   @param left x value for left side of screen relative to origin.
   @param right x value for right side of screen.
   @param top y value for top of screen.
-  @param bottom how far -y the screen extends.
+  @param bottom y value for bottom of screen.
   @param far far clipping plane. The near clipping plane will be the screen.
-  @param eye eye point.
+  @param eye pointer to eye vector.
   @param mat matrix output, row major.
 */
-
 void screen_proj_mat(float left, float right, float top, float bottom, float far, float *eye, float *mat);
 #endif
